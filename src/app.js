1df2821b679d96c42374f554cda17b3bf2ba40b6
GloryRojas <grojasm94@gmail.com>
2019-05-21 15:56:26 -0500

    Mostrando tareas desde mongodb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const path = require('path')
+const mongoose = require('mongoose');
 
-app.listen(4000, () =>{
-    console.log('Server on port 4000');
+// coneccting ti db
+
+mongoose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log('Db connected'))
+    .catch(err => console.log(err))
+
+// importing routes
+const indexRoutes = require('./routes/index')
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}))
+
+//routes
+app.use('/', indexRoutes);
+
+//starting the server
+app.listen(app.get('port'), () =>{
+    console.log(`Server on port ${app.get('port')} `);
 });
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const Task = require('../models/task');
+
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
+    console.log(tasks);
+    res.render('index', {
+        tasks
+    });
+});
+
+
+router.post('/add', async (req, res) => {
+    const task = new Task(req.body)
+    await task.save();
+    res.redirect('/')
+})
+
+module.exports = router;
